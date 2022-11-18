c91e2aa643679bab624f1d7d422122d25e8690a4
Hieu Nguyen <nguyenluongtrunghieu1995@gmail.com>
2022-11-18 16:23:53 +0700

    Implement config for deployment

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,6 +3,9 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Generate new app key"
+php artisan key:generate
+
 echo "Caching config..."
 php artisan config:cache
 
@@ -13,4 +16,4 @@ echo "Create the symbolic links configured for the application"
 php artisan storage:link
 
 echo "Running migrations..."
-php artisan migrate:fresh --force --seed
+php artisan migrate:fresh --force
